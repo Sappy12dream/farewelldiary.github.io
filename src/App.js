7cd4e7c30aa08d0afc2ba77678852450c16f8a20
sappy12dream <sapnaspy@gmail.com>
2020-09-21 18:50:11 +0530

    rest

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./styles/App.scss";
+
+//componentes
+import Header from "./component/Header"
+import Home from './Pages/Home';
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
